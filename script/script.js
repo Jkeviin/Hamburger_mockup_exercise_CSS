c1008b4162c6a66194901b0de76431b6593d1b7e
Kevin <slash2130kevin@gmail.com>
2022-12-05 16:03:02 -0500

    Quitar SetTimeOut cuando este en celular o tablet

--- script/script.js
+++ script/script.js
@@ -1,50 +1,58 @@
-// Path: script\script.js
-let scrollH = document.querySelector('.inner');
+/* Crear una funcion que mueva automaticamente un slide
 
-/* Funcion para hacer scroll horizontal */
-const scroll2 = () => {
+    Existe un componente llamado .inner con 3 componentes hijos los cuales se mueven como si fueran un slide,
 
-    if ((Math.round(scrollH.scrollLeft) + 1) >= scrollH.scrollWidth - scrollH.clientWidth) {
-        scrollH.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: 'smooth'
-        });
-    }
-
-    /* si no llega al maximo, sumar 1 */
-    else {
-        scrollH.scrollBy({
+    1. Se mueva infinitamente a la derecha con inner.scrollBy({
             top: 0,
             left: 10,
             behavior: 'smooth'
-        });  /* ScrollBy: desplazamiento relativo */
+    2. Cuando llegue al final del ultimo componente, el left vuelva a 0 el scrollTo
+    2. Solo se mueva si la mediaQuery es mayor de 1000px, de lo contrario no mover el scroll */
+
+const inner = document.querySelector(".inner");
+const mediaQuery = window.matchMedia("(min-width: 868px)");
+
+function moveSlide() {
+    if (mediaQuery.matches) {
+        if ((Math.round(inner.scrollLeft) + 1) >= inner.scrollWidth - inner.clientWidth) {
+            inner.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: 'smooth'
+            });
+        }
+        /* si no llega al maximo, sumar 1 */
+        else {
+            inner.scrollBy({
+                top: 0,
+                left: 10,
+                behavior: 'smooth'
+            });  /* ScrollBy: desplazamiento relativo */
+        }
     }
-
-    setTimeout(() => {
-        requestAnimationFrame(scroll2);
-    }, 5000);
-
-
 
 }
 
-scroll2();
+setInterval(moveSlide, 4000);
 
-/* PAra quitar el lag del setTimeout:
 
-    requestAnimationFrame(scroll2);
 
-    
-    requestAnimationFrame: es una funcion que se ejecuta cada vez que el navegador esta listo para pintar un nuevo frame en la pantalla.
 
-    requestAnimationFrame junto con SetTimeout:
 
-    setTimeout(() => {
-        requestAnimationFrame(scroll2);
-    }, 2000);
 
-*/
+
+
+
+
+
+
+
+// Path: script\script.js
+
+
+
+
+
 
 
 //////////
